3bfcd18fac030dd4ebed7bba4b0a1b590abddd10
takuseno <takuma.seno@gmail.com>
2024-11-24 17:20:19 +0900

    Add wait

--- src/main.c
+++ src/main.c
@@ -1,13 +1,24 @@
 #include "uart.h"
-#include "rand.h"
+#include "delays.h"
 
 void main() {
     uart_init();
-    rand_init();
 
-    uart_puts("Here goes a random number: ");
-    uart_hex(rand(0,4294967295));
-    uart_puts("\n");
+    uart_puts("Waitinig 1000000 CPU cycles (ARM CPU): ");
+    wait_cycles(1000000);
+    uart_puts("OK\n");
+
+    uart_puts("Waitinig 1000000 microsec (ARM CPU): ");
+    wait_msec(1000000);
+    uart_puts("OK\n");
+
+    uart_puts("Waiting 1000000 microsec (BCM System Timer): ");
+    if (get_system_timer() == 0) {
+        uart_puts("Not available\n");
+    } else {
+        wait_msec_st(1000000);
+        uart_puts("OK\n");
+    }
 
     // echo everything back
     while (1) {
