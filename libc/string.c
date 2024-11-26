87172b4656a1f8b067c2795c4efb26c5050296f2
takuseno <takuma.seno@gmail.com>
2024-11-26 23:04:31 +0900

    Remove unused variable

--- libc/string.c
+++ libc/string.c
@@ -116,7 +116,6 @@ char* strcpy(char *dest, const char* src) {
 
 char* strstr(const char *str1, const char *str2) {
     int str1_len = strlen(str1);
-    int str2_len = strlen(str2);
     for (int i = 0; i < str1_len; ++i) {
         if (strcmp(str1 + i, str2) == 0) {
             return (char*) str1 + i;
